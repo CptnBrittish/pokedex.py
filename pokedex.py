714ae640319e9d62738f045ee6cb577d9a56c784
Thomas Atkinson <cptn.brittish@gmail.com>
2014-12-20 19:48:39 +1100

    Added types to displayed information

--- pokedex.py
+++ pokedex.py
@@ -21,17 +21,35 @@ class Application(Frame):
         sprite_file.write(sprite_url)
         sprite_file.close()
 
+        #make sure all sprites are the same size
+        img = Image.open('temp.jpg')
+        size = 120, 120
+        img.thumbnail(size)
+        img.save('temp', "JPEG")
+        
         image = Image.open('temp.jpg')
         sprite = ImageTk.PhotoImage(image)
+
         self.poke_sprite_display = Canvas(width=120, height=120, bg='white')
-        self.poke_sprite_display.create_image(60, 40, image=sprite)
+        self.poke_sprite_display.create_image(60, 60, image=sprite)
         self.poke_sprite_display.image = sprite
-        self.poke_sprite_display.grid(column=0, columnspan = 4, row=0, rowspan = 4)
+        self.poke_sprite_display.grid(column=0, row=0, rowspan = 8)
         
         self.pokemon_name_label = Label(text="Pokemon Name:")
-        self.pokemon_name_label.grid(column=4, row=0)
+        self.pokemon_name_label.grid(column=1, row=0)
         self.pokemon_name = Label(text=pokemon.name)
-        self.pokemon_name.grid(column=5, row=0)
+        self.pokemon_name.grid(column=2, row=0)
+
+        self.pokemon_type_label = Label(text="Types:", anchor=S)
+        self.pokemon_type_label.grid(column=1, row=1)
+        pokemon_types = str()
+        
+        poke_types = list(pokemon.types)
+        pokemon_types = poke_types[0]
+        for type in poke_types[1:]:
+            pokemon_types = pokemon_types + ", " +  type
+        self.pokemon_type = Label(text=pokemon_types)
+        self.pokemon_type.grid(column=2, row=1)
         
 
 def get_pokemon(poke_name):
