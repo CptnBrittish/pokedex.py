f94b74f8d18f0344feafcc9fe67cc27003bb17e2
Thomas <18033120@student.uws.edu.au>
2015-01-08 19:38:32 +1100

    Add most relevant pokemon statistics to gui Split get_pokemon_description into a method

--- pokedex.py
+++ pokedex.py
@@ -54,15 +54,27 @@ class Application(Frame):
         self.poke_sprite_display = Canvas(self.pokemon_frame, width=120, height=120, bg='white')
         self.poke_sprite_display.create_image(60, 60, image=sprite)
         self.poke_sprite_display.image = sprite
-        self.poke_sprite_display.grid(column=0, row=0, rowspan = 8)
-        
-        self.pokemon_name_label = Label(self.pokemon_frame, text="Pokemon Name:")
-        self.pokemon_name_label.grid(column=1, row=0)
-        self.pokemon_name = Label(self.pokemon_frame, text=pokemon.name)
-        self.pokemon_name.grid(column=2, row=0)
+        self.poke_sprite_display.grid(column=0,columnspan=4, row=0, rowspan = 11)
 
-        self.pokemon_type_label = Label(self.pokemon_frame, text="Types:", anchor=S)
-        self.pokemon_type_label.grid(column=1, row=1)
+        #Pokemon name
+        self.pokemon_name_label = Label(self.pokemon_frame, text="Name:")
+        self.pokemon_name_label.grid(column=0, row=9)
+        self.pokemon_name = Label(self.pokemon_frame, text=pokemon.name)
+        self.pokemon_name.grid(column=1, row=9)
+
+        #pokemon gender
+        self.pokemon_gender_label = Label(self.pokemon_frame, text="Gender:")
+        self.pokemon_gender_label.grid(column=0, row=10)
+
+        #pokemon national id
+        self.pokemon_number_label = Label(self.pokemon_frame, text="#")
+        self.pokemon_number_label.grid(column=3, row=10)
+        self.pokemon_number = Label(self.pokemon_frame, text=pokemon.id)
+        self.pokemon_number.grid(column = 4, row = 10)
+
+        #Pokemon types
+        self.pokemon_type_label = Label(self.pokemon_frame, text="Type:", anchor=S)
+        self.pokemon_type_label.grid(column=5, row=0)
         pokemon_types = str()
         
         poke_types = list(pokemon.types)
@@ -70,8 +82,50 @@ class Application(Frame):
         for type in poke_types[1:]:
             pokemon_types = pokemon_types + ", " +  type
         self.pokemon_type = Label(self.pokemon_frame, text=pokemon_types)
-        self.pokemon_type.grid(column=2, row=1)
+        self.pokemon_type.grid(column=6, row=0)
+
+        #Pokemon Species
+        self.pokemon_species_label = Label(self.pokemon_frame, text="Species:")
+        self.pokemon_species_label.grid(column=5, row=1)
+        self.pokemon_species = Label(self.pokemon_frame, text=pokemon.species)
+        self.pokemon_species.grid(column=6, row=1)
+
+        #height and weight
+        self.pokemon_height_label = Label(self.pokemon_frame, text="Height:")
+        self.pokemon_height_label.grid(column=5, row=2)
+        self.pokemon_height = Label(self.pokemon_frame, text=pokemon.height)
+        self.pokemon_height.grid(column=6, row=2)
+        self.pokemon_weight_label = Label(self.pokemon_frame, text="Weight:")
+        self.pokemon_weight_label.grid(column=7, row=2)
+        self.pokemon_weight = Label(self.pokemon_frame, text=pokemon.weight)
+        self.pokemon_weight.grid(column=8, row=2)
+
+        #Pokemon Abilities
+        self.pokemon_abilities_label = Label(self.pokemon_frame, text="Abilities:")
+        self.pokemon_abilities_label.grid(column=5, row=3)
+        self.pokemon_abilities = Label(self.pokemon_frame, text=pokemon.abilities)
+
+        #Pokemon weaknesses
+        self.pokemon_weaknesses_label = Label(self.pokemon_frame, text="Weaknesses:")
+        self.pokemon_weaknesses_label.grid(column=5, row=4)
+
+        #Stats
+        self.pokemon_hp_label = Label(self.pokemon_frame, text="HP")
+        self.pokemon_hp_label.grid(column=5, row=5)
+        self.pokemon_defense_label = Label(self.pokemon_frame, text="Defense")
+        self.pokemon_defense_label.grid(column=5, row=7)
+        self.pokemon_attack_label = Label(self.pokemon_frame, text="Attack")
+        self.pokemon_attack_label.grid(column=5, row=9)
+        self.pokemon_speed_label = Label(self.pokemon_frame, text="Speed")
+        self.pokemon_speed_label.grid(column=6, row=5)
+        self.pokemon_sp_defense_label = Label(self.pokemon_frame, text="Sp Defense")
+        self.pokemon_sp_defense_label.grid(column=6, row=7)
+        self.pokemon_sp_attack_label = Label(self.pokemon_frame, text="Sp Attack")
+        self.pokemon_sp_attack_label.grid(column=6, row=9)
         
+        #description
+        self.pokemon_description = Label(self.pokemon_frame, text=get_description(pokemon.descriptions[sorted(pokemon.descriptions.keys())[0]][20:-1]).description, wraplength=160)
+        self.pokemon_description.grid(column=5, row=10, columnspan=4, rowspan=4)
         
     def createWidgets(self):
         self.search_term = Entry(self.search_frame)
@@ -93,6 +147,9 @@ def get_sprite(poke_num):
     pokemon_sprite = pykemon.get(sprite=poke_num)
     return pokemon_sprite
 
+def get_description(poke_des):
+    pokemon_description = pykemon.get(description=poke_des)
+    return pokemon_description
 
 if __name__ == "__main__":
     root = Tk()
